69d219a39ec3fed54d9940f73d5cd51e6cd802b6
Lateralus <zoofoo.online@gmail.com>
2021-03-30 22:45:43 +0100

    set up done

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
