f327982a1df49d858b68004672be1fbc09ac1559
Lateralus <zoofoo.online@gmail.com>
2021-04-01 23:14:47 +0100

    restructured archetecture of class names

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -4,12 +4,12 @@ const Messenger = () => {
   return (
     <div className="messenger">
       <div className="messenger__bar">Messenger</div>
-      <div className="messenger__header">
-        <div className="header">
-          <div className="header__item">
-            <div className="header__title">Room</div>
-            <div className="header__online">2 users online</div>
-          </div>
+      <div className="header">
+        <div className="header__left">
+          <div className="header__title">Room</div>
+          <div className="header__online">2 users online</div>
+        </div>
+        <div className="header__right">
           <div className="burger">
             <div className="burger__line"></div>
             <div className="burger__line"></div>
@@ -17,11 +17,9 @@ const Messenger = () => {
           </div>
         </div>
       </div>
-      <div className="messenger__chat">Chat</div>
-      <div className="messenger__textarea">Textarea</div>
-      <div className="messenger__sidebar">
-        <Sidebar />
-      </div>
+      <div className="chat">Chat</div>
+      <div className="text">Textarea</div>
+      <Sidebar />
     </div>
   );
 };
