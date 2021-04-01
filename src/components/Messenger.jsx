daf42028b2d249da6bbd5ec642d8c9562d926657
Lateralus <zoofoo.online@gmail.com>
2021-04-01 01:40:06 +0100

    styling sidebar

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,11 +1,27 @@
+import Sidebar from './Sidebar';
+
 const Messenger = () => {
   return (
     <div className="messenger">
-      <div className="messenger__bar">Bar</div>
-      <div className="messenger__header">Header</div>
+      <div className="messenger__bar">Messenger</div>
+      <div className="messenger__header">
+        <div className="header">
+          <div className="header__item">
+            <div className="header__title">Room</div>
+            <div className="header__online">2 users online</div>
+          </div>
+          <div className="burger">
+            <div className="burger__line"></div>
+            <div className="burger__line"></div>
+            <div className="burger__line"></div>
+          </div>
+        </div>
+      </div>
       <div className="messenger__chat">Chat</div>
       <div className="messenger__textarea">Textarea</div>
-      <div className="messenger__sidebar">Sidebar</div>
+      <div className="messenger__sidebar">
+        <Sidebar />
+      </div>
     </div>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__search">
+        <input className="input__search" placeholder="Search..." />
+      </div>
+      <div className="sidebar__user">Eli Krizevski</div>
+    </div>
+  );
+};
+
+export default Sidebar;
