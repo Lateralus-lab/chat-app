d8fe45d29cf85c2d753482e512affa1362db7769
Lateralus <zoofoo.online@gmail.com>
2021-04-16 22:58:00 +0100

    chat app completed

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,47 @@
 import { useState } from 'react';
 import firebase from '../firebase';
-import Authorize from './Authorize';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import logo from '../assets/img/logo.png';
 
-const LoginForm = () => {
+const LoginForm = ({ setIsRegistered }) => {
+  const [showError, setShowError] = useState('');
+
   const auth = firebase.auth();
 
-  const [name, setName] = useState('');
-  const [nickName, setNickName] = useState('');
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    touched,
+    errors,
+    handleBlur,
+  } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Email is invalid')
+        .required('Email is required'),
+      password: Yup.string()
+        .min(6, 'Password must be at least 6 characters')
+        .required('Password is required'),
+    }),
+    onSubmit: ({ email, password }) => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((signedInUser) => {
+          console.log(signedInUser);
+        })
+        .catch((err) => {
+          console.log(err);
+          setShowError(err.message);
+        });
+    },
+  });
 
   const signInWithGoogle = async () => {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -19,37 +54,71 @@ const LoginForm = () => {
     }
   };
 
+  const handleUser = (e) => {
+    e.preventDefault();
+
+    setIsRegistered(false);
+  };
+
   return (
     <div className="wrapper">
-      <div className="form">
-        <Authorize />
-        <div className="input__group">
-          <input
-            className="input"
-            type="text"
-            placeholder="Name"
-            onChange={(e) => setName(e.target.value)}
-          />
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="authorize">
+          <div className="authorize__img">
+            <img className="logo__img" alt="logo" src={logo} />
+          </div>
+          <h3 className="authorize__title">Authorization</h3>
+          <div className="authorize__desc">
+            Please enter your email and password for further authorization
+          </div>
         </div>
         <div className="input__group">
           <input
             className="input"
-            type="text"
-            placeholder="Nickname"
-            onChange={(e) => setNickName(e.target.value)}
+            type="email"
+            name="email"
+            placeholder="Email"
+            values={values.email}
+            onBlur={handleBlur}
+            onChange={handleChange}
           />
+          {touched.email && errors.email ? (
+            <div className="input__label">{errors.email}</div>
+          ) : null}
         </div>
+        <div className="input__group">
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            values={values.password}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {touched.password && errors.password ? (
+            <div className="input__label">{errors.password}</div>
+          ) : null}
+        </div>
+        {showError ? <div className="error">{showError}</div> : null}
         <div className="centered">
           <button className="btn" type="submit">
             Login
           </button>
-          <div>
-            <button className="btn btn-dark" onClick={signInWithGoogle}>
-              Sign in with Google
-            </button>
-          </div>
+          <div></div>
         </div>
-      </div>
+        <div className="centered centered-google">
+          <button className="btn btn-dark" onClick={signInWithGoogle}>
+            Login with Google
+          </button>
+        </div>
+        <div className="form__user">
+          Don't have an account?
+          <a className="form__link" href="/" onClick={handleUser}>
+            Register
+          </a>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/RegForm.jsx
+++ src/components/RegForm.jsx
@@ -1,71 +1,92 @@
 import { useState } from 'react';
 import firebase from '../firebase';
-import Authorize from './Authorize';
+import md5 from 'md5';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import logo from '../assets/img/logo.png';
 
-const RegForm = () => {
-  const [nickname, setNickname] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passConfirmation, setPassConfirmation] = useState('');
+const RegForm = ({ setIsRegistered }) => {
+  const [showError, setShowError] = useState('');
 
-  const isFormValid = (nickname, email, password, passConfirmation) => {
-    if (isFormEmpty(nickname, email, password, passConfirmation)) {
-      // thorow error
-    } else if (isPasswordValid(password, passConfirmation)) {
-      // Throw erroe
-    } else {
-      return true;
-    }
-  };
-
-  const isFormEmpty = (nickname, email, password, passConfirmation) => {
-    return (
-      !nickname.length ||
-      !email.length ||
-      !password.length ||
-      !passConfirmation.length
-    );
-  };
-
-  const isPasswordValid = (password, passConfirmation) => {
-    if (password.length < 6 || passConfirmation < 6) {
-      return false;
-    } else if (password !== passConfirmation) {
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (isFormValid(nickname, email, password, passConfirmation)) {
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    touched,
+    errors,
+    handleBlur,
+  } = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+      email: Yup.string()
+        .email('Email is invalid')
+        .required('Email is required'),
+      password: Yup.string()
+        .min(6, 'Password must be at least 6 characters')
+        .required('Password is required'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password'), null], 'Password must match')
+        .required('Password confirmation is required'),
+    }),
+    onSubmit: ({ username, email, password }) => {
       firebase
         .auth()
         .createUserWithEmailAndPassword(email, password)
         .then((createdUser) => {
-          console.log(createdUser);
+          createdUser.user.updateProfile({
+            displayName: username,
+            photoURL: `http://gravatar.com/avatar/${md5(
+              createdUser.user.email
+            )}?d=identicon`,
+          });
         })
         .catch((err) => {
           console.log(err);
+          setShowError(err.message);
         });
-    }
+    },
+  });
+
+  const handleUser = (e) => {
+    e.preventDefault();
+
+    setIsRegistered(true);
   };
 
   return (
     <div className="wrapper">
       <form className="form" onSubmit={handleSubmit}>
-        <Authorize />
+        <div className="authorize">
+          <div className="authorize__img">
+            <img className="logo__img" alt="logo" src={logo} />
+          </div>
+          <h3 className="authorize__title">Registration</h3>
+          <div className="authorize__desc">
+            Please enter your details to register a new user and for further
+            authorization
+          </div>
+        </div>
         <div className="input__group">
           <input
             className="input"
             type="text"
-            name="name"
-            placeholder="Nickname"
-            value={nickname}
-            onChange={(e) => setNickname(e.target.value)}
+            name="username"
+            placeholder="Username"
+            values={values.usename}
+            onBlur={handleBlur}
+            onChange={handleChange}
           />
+          {touched.username && errors.username ? (
+            <div className="input__label">{errors.username}</div>
+          ) : null}
         </div>
         <div className="input__group">
           <input
@@ -73,9 +94,13 @@ const RegForm = () => {
             type="email"
             name="email"
             placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            values={values.email}
+            onBlur={handleBlur}
+            onChange={handleChange}
           />
+          {touched.email && errors.email ? (
+            <div className="input__label">{errors.email}</div>
+          ) : null}
         </div>
         <div className="input__group">
           <input
@@ -83,25 +108,40 @@ const RegForm = () => {
             type="password"
             name="password"
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            values={values.password}
+            onBlur={handleBlur}
+            onChange={handleChange}
           />
+          {touched.password && errors.password ? (
+            <div className="input__label">{errors.password}</div>
+          ) : null}
         </div>
         <div className="input__group">
           <input
             className="input"
             type="password"
-            name="password"
-            placeholder="Password"
-            value={passConfirmation}
-            onChange={(e) => setPassConfirmation(e.target.value)}
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            values={values.confirmPassword}
+            onBlur={handleBlur}
+            onChange={handleChange}
           />
+          {touched.confirmPassword && errors.confirmPassword ? (
+            <div className="input__label">{errors.confirmPassword}</div>
+          ) : null}
         </div>
+        {showError ? <div className="error">{showError}</div> : null}
         <div className="centered">
           <button className="btn" type="submit">
             Register
           </button>
           <div></div>
+        </div>
+        <div className="form__user">
+          Already a user?
+          <a className="form__link" href="/" onClick={handleUser}>
+            Login
+          </a>
         </div>
       </form>
     </div>
